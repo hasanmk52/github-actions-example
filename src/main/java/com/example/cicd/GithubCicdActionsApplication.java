c2ca014fc8075a176f331c8135b7b5cfa1d717f2
Hasan Kagalwala <hasanmk52@gmail.com>
2021-11-13 22:56:07 +0400

    Modified sample test endpoint.

--- src/main/java/com/example/cicd/GithubCicdActionsApplication.java
+++ src/main/java/com/example/cicd/GithubCicdActionsApplication.java
@@ -11,16 +11,9 @@ public class GithubCicdActionsApplication {
 
 	@GetMapping("/welcome")
 	public String welcome(){
-		return "Welcome to javatechie !";
+		return "Welcome to my app!";
 	}
 
-//	echo "# github-actions-example" >> README.md
-//	git init
-//	git add README.md
-//	git commit -m "first commit"
-//	git branch -M main
-//	git remote add origin https://github.com/Java-Techie-jt/github-actions-example.git
-//	git push -u origin main
 	public static void main(String[] args) {
 		SpringApplication.run(GithubCicdActionsApplication.class, args);
 	}
